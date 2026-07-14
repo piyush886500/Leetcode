d1e8f88105ad5857072a3b682de94f21d837e673
Piyush Kumar <155344190+piyush886500@users.noreply.github.com>
2026-07-14 21:51:06 +0530

    Move Zeroes updated

--- Move_Zeoes.java
+++ Move_Zeoes.java
@@ -1,5 +1,5 @@
-class Solution {
-    public void moveZeroes(int[] nums) {
+class Move_Zeoes {
+    public static void moveZeroes(int[] nums) {
         int l = 0;
         int size = nums.length;
         for(int r =0;r<size;r++){
@@ -10,5 +10,10 @@ class Solution {
                 l++;
             }    
     }
+    System.out.println(l);
 }
+    public static void main(String[] args) {
+        int nums[] = {2,5,6,1};
+        moveZeroes(nums);
+    }
 }
